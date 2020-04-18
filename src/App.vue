f725b733e6150345052b30f213f7da1fd9cd952c
NiVanc <nico@izebra.be>
2020-04-18 21:11:45 +0200

    Use dynamic names

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,15 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr />
+        <select v-model="animationStyle" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br />
         <button class="btn btn-primary" @click="show = !show">Show alert</button>
         <br />
         <br />
-        <transition name="fade">
+        <transition :name="animationStyle">
           <div class="alert alert-info" v-if="show">This is a info alert, check it out!</div>
         </transition>
         <transition name="slide" type="animation">
@@ -25,7 +30,8 @@
 export default {
   data() {
     return {
-      show: true
+      show: true,
+      animationStyle: "fade"
     };
   }
 };
